22b4a759dbc36f33cde432b35a02ba7770423da7
jahiduloo7 <jahidul479@gmail.com>
2021-01-11 15:28:45 +0600

    detail

--- lib/screens/detail_page.dart
+++ lib/screens/detail_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/bloc/get_movies_video_bloc.dart';
 import 'package:movie_app/model/movie.dart';
+import 'package:movie_app/model/video.dart';
+import 'package:movie_app/model/video_response.dart';
 import 'package:movie_app/style/theme.dart' as Style;
+import 'package:movie_app/widgets/buildErrorWidget.dart';
+import 'package:movie_app/widgets/buildLoadingWidget.dart';
+import 'package:sliver_fab/sliver_fab.dart';
 
 class MovieDetailScreen extends StatefulWidget {
   final Movie movie;
@@ -12,14 +18,109 @@ class MovieDetailScreen extends StatefulWidget {
 }
 
 class _MovieDetailScreenState extends State<MovieDetailScreen> {
-
   final Movie movie;
-
 
   _MovieDetailScreenState(this.movie);
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    movieVideosBloc..getMovieVideo(movie.id);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    movieVideosBloc..drainStream();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Style.Colors.mainColor,
+      body: Builder(
+        builder: (context) {
+          return SliverFab(
+            floatingPosition: FloatingPosition(right: 20.0),
+            floatingWidget: StreamBuilder<VideoResponse>(
+              stream: movieVideosBloc.subject.stream,
+              builder: (context, AsyncSnapshot<VideoResponse> snapshot) {
+                if (snapshot.hasData) {
+                  if (snapshot.data.error != null &&
+                      snapshot.data.error.length > 0) {
+                    return buildErrorWidget(snapshot.data.error);
+                  }
+                  return _buildVideoWidget(snapshot.data);
+                } else if (snapshot.hasError) {
+                  return buildErrorWidget(snapshot.error);
+                } else {
+                  return buildLoadingWidget();
+                }
+              },
+            ),
+            expandedHeight: 200,
+            slivers: <Widget>[
+              SliverAppBar(
+                backgroundColor: Style.Colors.mainColor,
+                expandedHeight: 200,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text(
+                    movie.title.length > 40
+                        ? movie.title.substring(0, 37) + "..."
+                        : movie.title,
+                    style:
+                        TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
+                  ),
+                  background: Stack(
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://image.tmdb.org/t/p/original/" +
+                                    movie.backPoster),
+                          ),
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                              Colors.black.withOpacity(0.9),
+                              Colors.black.withOpacity(0.0),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildVideoWidget(VideoResponse data) {
+    List<Video> videos = data.videos;
+    return FloatingActionButton(
+      backgroundColor: Style.Colors.secondColor,
+      child: Icon(Icons.play_arrow),
+      onPressed: null,
+    );
   }
 }
